7fcb3e0538d24d5bea79dc3dd554b9c99d6ec421
PC <stefan.bogai01@e-uvt.ro>
2023-04-10 21:23:54 +0300

    Configured DB Connection

--- backend/server.js
+++ backend/server.js
@@ -97,3 +97,27 @@ server.listen(9000, "127.0.0.1", () => {
     const addr = server.address();
 		console.log(`http://${addr.address}:${addr.port}`);
 });
+
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your connection string.
+const uri = "<mongodb+srv://bogai_tudor:<wfrVHIgo6J0F7y68>@cluster0.zrv1qdz.mongodb.net/?retryWrites=true&w=majority>";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
